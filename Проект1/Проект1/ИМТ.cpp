dcfe4182dafc77fc5c5f12669a78f6ef5d024874
Renat7797 <zamanov.renat@mail.ru>
2018-03-23 13:26:11 +0300

    Выполнил 1 часть ДЗ по расчету ИМТ

---
+++ Проект1/Проект1/ИМТ.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <stdio.h> 
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b;
+	cout << "Введите Ваш вес в килограммах ";
+	cin >> a;
+	cout << "Введите Ваш рост в cантиметрах ";
+	cin >> b;
+	double c = a / pow((b / 100), 2);//Высчитываем значение ИМТ
+	cout << "Индекс массы вашего тела (ИМТ) = " << c << endl; // вывод ответа. 
+	system("pause");
+	return 0;
+}
